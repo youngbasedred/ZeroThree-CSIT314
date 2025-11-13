2547e46dc4e8e2a6963d154dba665cf1a68a3acc
Andre <126609840+youngbasedred@users.noreply.github.com>
2025-11-13 23:36:39 +0800

    Delete ZeroThree/UserStory01 directory

--- ZeroThree/UserStory01/UserStory15/update_request.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/Boundary/updateMyRequestBoundary.php';
-
-$userID = 11; // Your PIN user
-
-$boundary = new updateMyRequestBoundary();
-$boundary->displayUpdateRequest($userID);
-?>
